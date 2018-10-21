22bc395aba30c6828738cb9d6af11f8688070df6
Your Name <paul.joireman@gmail.com>
2018-10-21 18:48:36 -0400

    Adding linting capability

--- scripts/srcServer.js
+++ scripts/srcServer.js
@@ -12,13 +12,23 @@ app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
 }))
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    // Hard code for simplicity
+    res.json([
+        {"id":1,"firstName":"Carl","lastName":"Jones","email":"carl.jones2134@gmail.com"},
+        {"id":2,"firstName":"Sid","lastName":"Finster","email":"sid.finster@aol.com"},
+        {"id":3,"firstName":"Matrim","lastName":"Cauthon","email":"matt.c@hotmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
-        console.log(err);
+        console.log(err);   // eslint-disable-line no-console
     } else {
         open('http://localhost:' + port);
     }
