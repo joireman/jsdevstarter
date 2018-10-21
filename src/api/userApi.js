5524f253ede90587b2557314f6631e96cb74df50
Your Name <paul.joireman@gmail.com>
2018-10-21 19:01:52 -0400

    Added basic mock APi

---
+++ src/api/userApi.js
@@ -0,0 +1,18 @@
+/* Centralized API calls */
+import 'whatwg-fetch';
+
+export function getUsers() {
+  return get('users');
+}
+
+function get(url) {
+  return fetch(url).then(onSuccess, onError);
+}
+
+function onSuccess(res) {
+  return res.json();
+}
+
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('index.html', () => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.contains('Hello');
+      expect(h1.innerHTML).to.contains('Users');
       done();
       window.close();
     });
